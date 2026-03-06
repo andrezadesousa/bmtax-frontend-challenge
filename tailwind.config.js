661782db7e3bda6a5546fd67d774f46ff07a4d77
Andreza de Sousa <andrezanascimentodesousa@gmail.com>
2026-03-06 12:47:16 -0300

    feat(tailwind.config): added tailwind configuration to the project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#391B64",
+          dark: "#391B64",
+          medium: "#50268C",
+          light: "#6D28D9",
+        },
+        surface: {
+          light: "#F5F3FF",
+          white: "#FFFFFF",
+        },
+        text: {
+          dark: "#111827",
+          white: "#FFFFFF",
+        },
+      },
+      borderRadius: {
+        full: "999px",
+      },
+    },
   },
   plugins: [],
 };
