4c7f3389bcdd925a7a85ac3dc69edb19f546757a
Andreza de Sousa <andrezanascimentodesousa@gmail.com>
2026-03-08 19:46:37 -0300

    chore(item): define Item type with id name industry location and thumbnail fields

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,4 +1,10 @@
 export type Item = {
   id: number;
   name: string;
+  description: string;
+  founded: number;
+  industry: string;
+  headquarters: string;
+  location: string;
+  thumbnail: string;
 };
